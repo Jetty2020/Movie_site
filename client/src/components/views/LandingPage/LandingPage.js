3be323a2295804c5d79b03128d5dda52fa8ffc3a
Jetty2020 <kmclo@naver.com>
2021-03-02 03:16:55 +0900

    2021-3-2  create  GridCards.js

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from 'react'
 import { API_URL, IMAGE_BASE_URL } from '../../Config';
 import MainImage from './Sections/MainImage';
 import env from "react-dotenv";
-import axios from 'axios';
+import GridCards from '../commons/GridCards';
+import { Row } from 'antd';
 
 
 function LandingPage() {
@@ -11,14 +12,11 @@ function LandingPage() {
 
 	useEffect(() => {
 		const endpoint = `${API_URL}movie/popular?api_key=${env.API_KEY}&language=en-US&page=1`;
-		// axios.get(endpoint)
-		// 	.then(response => {
-		// 		setMovies([...response.results])
-		// 		setMainMovieImage(response.results[0])
-		// 	})		
+			
 		fetch(endpoint)
 			.then(response => response.json())
-			.then(response => {                
+			.then(response => {
+				console.log(response.results)
 				setMovies([...response.results])
 				setMainMovieImage(response.results[0])
 			})
@@ -27,6 +25,7 @@ function LandingPage() {
 	return (
 		<div style={{ width: '100%', margin: '0' }}>
 			{/* Main Image */}
+			{/* something && action ,something이 있으면 action 보여줘라 */}
 			{MainMovieImage &&
 				<MainImage
 					image={`${IMAGE_BASE_URL}w1280${MainMovieImage.backdrop_path}`}
@@ -38,6 +37,18 @@ function LandingPage() {
 				<h2>Movies by latest</h2>
 				<hr />
 				{/* Movie Grid Cards */}
+				<Row gutter={[16, 16]} >
+          {Movies && Movies.map((movie, index) => (
+            <React.Fragment key={index}>
+              <GridCards
+                image={movie.poster_path ?
+                  `${IMAGE_BASE_URL}w500${movie.poster_path}` : null}
+                movieId={movie.id}
+                movieName={movie.original_title}
+              />
+            </React.Fragment>
+          ))}
+        </Row>
 			</div>
 			<div style={{ display: 'flex', justifyContent: 'center' }}>
 				<button> Load More</button>
