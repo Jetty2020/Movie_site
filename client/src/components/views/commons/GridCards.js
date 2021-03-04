37b03571eb322687efedb855b356d81d86c40233
Jetty2020 <kmclo@naver.com>
2021-03-04 10:01:31 +0900

    2021-3-4 MovieDetail.js getting actor info

--- client/src/components/views/commons/GridCards.js
+++ client/src/components/views/commons/GridCards.js
@@ -1,18 +1,34 @@
-import React from 'react'
-import { Col } from 'antd';
-
+import React from "react";
+import { Col } from "antd";
 
 function GridCards(props) {
-  return (
-    // col은 총 24개의 column으로 이루어져 있음
-    <Col lg={6} md={8} sm={12} xs={24}>
-      <div style={{ position: 'relative' }}>
-        <a href={`/movie/${props.movieId}`} >
-          <img style={{ width: '100%', height: '320px' }} src={props.image} alt={props.movieName} />
-        </a>
-      </div>
-    </Col>
-  )
+  if (props.landingPage) {
+    return (
+      <Col lg={6} md={8} xs={24}>
+        <div style={{ position: "relative" }}>
+          <a href={`/movie/${props.movieId}`}>
+            <img
+              style={{ width: "100%", height: "320px" }}
+              src={props.image}
+              alt={props.movieName}
+            />
+          </a>
+        </div>
+      </Col>
+    );
+  } else {
+    return (
+      <Col lg={6} md={8} xs={24}>
+        <div style={{ position: "relative" }}>
+          <img
+            style={{ width: "100%", height: "320px" }}
+            src={props.image}
+            alt={props.characterName}
+          />
+        </div>
+      </Col>
+    );
+  }
 }
 
 export default GridCards;
